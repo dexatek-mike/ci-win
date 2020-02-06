81db00ec5b3543c86fa2776dfcd4881a5fcf1a56
DEXATEK\mike.chang <mike.chang@dexatek.com>
2020-02-06 20:42:58 +0800

    add necessary tools

--- gen_flash_bin.sh
+++ gen_flash_bin.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 #set -x
 
+PROJ_ROOT=$PWD
+PATH_FLASH_TOOL=$PROJ_ROOT/flash_tool_win
+PATH_UTILS=$PROJ_ROOT/utils
+PATH=$PATH:$PATH_FLASH_TOOL:$PATH_UTILS
+
 # programming settings for RTK flash tool to generate images
 DFT_IMG=ram_all.bin
 OTA_IMG=ota.bin
 MP_IMG=mp_ota.bin
 OTA_IMG_OFFSET=0xfb000
-CAL_IMG=A000.dat
+#CAL_IMG=A000.dat
+CAL_IMG=
 OUT_IMG=flash.bin
 DFT_BOOT=dftimg
 # programming settings for RTK flash tool to download images
@@ -18,5 +24,10 @@ PROG_KEEP_CAL=true
 PROG_KEEP_CAL_OFFSET=0x0
 PROG_AUTO_RESET=false
 
+pushd $PATH_FLASH_TOOL
+cp $PATH_ROOT/ota.bin $PATH_ROOT/ram_all.bin -r .
+cmd "/C 7z x JLink_V494.7z"
 # generate flash images
 ImageTool.exe -generate dftimg=$DFT_IMG otaimg=$OTA_IMG,offset=$OTA_IMG_OFFSET calimg=$CAL_IMG outimg=$OUT_IMG dftboot=$DFT_BOOT
+cp $OUT_IMG -r $PATH_ROOT
+popd
